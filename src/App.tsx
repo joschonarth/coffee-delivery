c7d1e8b9fb13f53eff239adf2a8784ebaaa1e870
joschonarth <joschonarth@gmail.com>
2025-06-13 19:23:54 -0300

    chore: setup global styles and themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
-function App() {
-  return <h1>Hello World</h1>
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
-
-export default App
