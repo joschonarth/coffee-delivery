1e51891397b0142745e8d396f5c777d740c0eba5
joschonarth <joschonarth@gmail.com>
2025-06-14 11:04:49 -0300

    feat: implement HeroContent to Home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 import { Header } from './components/Header'
+import { Home } from './pages/Home'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <Header />
+      <Home />
     </ThemeProvider>
   )
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,78 @@
+import {
+  CoffeeIcon,
+  PackageIcon,
+  ShoppingCartIcon,
+  TimerIcon,
+} from '@phosphor-icons/react'
+import { useTheme } from 'styled-components'
+
+import { Heading, Hero, HeroContent, Info } from './styles'
+
+export function Home() {
+  const theme = useTheme()
+
+  return (
+    <div>
+      <Hero>
+        <HeroContent>
+          <div>
+            <Heading>
+              <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+
+              <span>
+                Com o Coffee Delivery você recebe seu café onde estiver, a
+                qualquer hora
+              </span>
+            </Heading>
+
+            <Info>
+              <div>
+                <ShoppingCartIcon
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors['yellow-dark'] }}
+                />
+                <span>Compra simples e segura</span>
+              </div>
+
+              <div>
+                <PackageIcon
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors['base-text'] }}
+                />
+                <span>Embalagem mantém o café intacto</span>
+              </div>
+
+              <div>
+                <TimerIcon
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors.yellow }}
+                />
+                <span>Entrega rápida e rastreada</span>
+              </div>
+
+              <div>
+                <CoffeeIcon
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors.purple }}
+                />
+                <span>O café chega fresquinho até você</span>
+              </div>
+            </Info>
+          </div>
+
+          <img src="/images/hero.svg" alt="Café do Coffee Delivery" />
+        </HeroContent>
+
+        <img src="/images/hero-bg.svg" id="hero-bg" alt="" />
+      </Hero>
+    </div>
+  )
+}
