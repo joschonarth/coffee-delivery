e1a76cd61c8eb10bb6cb69caf595f8b25b72698a
joschonarth <joschonarth@gmail.com>
2025-06-15 12:47:24 -0300

    feat: create Cart page

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 import { Header } from './components/Header'
-import { Home } from './pages/Home'
+
+import { CartContextProvider } from './contexts/CartProvider'
+import { Outlet } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Header />
-      <Home />
+
+      <CartContextProvider>
+        <Header />
+        <Outlet />
+      </CartContextProvider>
     </ThemeProvider>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { Cart } from './pages/Cart'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
